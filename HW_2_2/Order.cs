ecc99753c21377f5c4e0b5ad22b9940c85e4562c
Бабко Евгений <doxis32123@gmail.com>
2022-12-12 21:23:56 +0200

    HW_2_2

---
+++ HW_2_2/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_2_2
+{
+    internal class Order
+    {
+        public static void OrderProcess()
+        {
+            Console.WriteLine();
+            Console.WriteLine("You order:");
+            Console.WriteLine("Order number: " + OrderNumber());
+            Cart.CartReader();
+            Console.WriteLine("Buyer information:");
+            Console.WriteLine(Customers._costumer);
+
+        }
+
+        private static int OrderNumber()
+        {
+            Random rand = new Random();
+            return rand.Next(100000,999999);
+        }
+    }
+}
---
+++ HW_2_2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HW_2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("List of available products:");
+            Goods.GoodList();
+            Console.WriteLine("Select products by entering numbers separated by space (max 10)");
+            Cart.CartReader();
+            Customers.NewCustomer();
+            Order.OrderProcess();
+        }
+    }
+}
